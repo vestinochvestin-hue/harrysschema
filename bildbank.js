1466a524d81c20bc85fcabd3da7e9e15a7a7be63
vestinochvestin-hue <vestinochvestin@gmail.com>
2026-07-31 15:03:48 +0200

    Add files via upload

---
+++ bildbank.js
@@ -0,0 +1,23 @@
+// Här lägger du enkelt till nya ord och bilder
+const IMAGE_BANK = {
+  'frukost': 'bilder/frukost.png',
+  'buss': 'bilder/aka_buss.png',
+  'åka buss': 'bilder/buss.png',
+  'pizza': 'bilder/pizza.png',
+  'mellis': 'bilder/mellis.png',
+  'lunch': 'bilder/lunch.png',
+  'skola': 'bilder/skola.png',
+  'hem': 'bilder/hem.png',
+  'bada': 'bilder/bada.png',
+  'handla': 'bilder/handla.png',
+  'padda': 'bilder/padda.png',
+  'lekpark': 'bilder/lekpark.png',
+  'gunga': 'bilder/gunga.png',
+  'sova': 'bilder/sova.png',
+  'affär': 'bilder/handla.png',
+  'snacks': 'bilder/snacks.png',
+  'middag': 'bilder/middag.png'
+};
+
+// Bilden som används om inget ord matchar
+const DEFAULT_IMAGE = 'https://api.iconify.design/noto:calendar.svg';
